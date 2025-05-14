3fddb29915fe5552f43793fab340a6f4add2d819
Thayanantham Kaliappan <thayanantham.k@mobiasia.com>
2025-05-14 11:25:41 +0530

    minor bug fix:watchlist

--- src/app/components/watchlist/watchlist.component.ts
+++ src/app/components/watchlist/watchlist.component.ts
@@ -1,8 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { StockService } from '../../services/stock.service';
-import { CommonModule, NgForOf, NgOptimizedImage } from '@angular/common';
+import { CommonModule, NgForOf } from '@angular/common';
 import { NavbarComponent } from '../../navbar/navbar.component';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+
+interface Stock {
+  stockId: number;
+  stockPrice: number;
+  companyName: string;
+  tickerSymbol: string;
+  exchange: string;
+  sector: string;
+  ipoQty: number;
+  desc: string | null;
+  imageURL: string;
+  createdAt: string;
+  updatedAt: string;
+  safeImageURL?: SafeUrl;
+}
 
 @Component({
   selector: 'app-watchlist',
@@ -11,26 +27,29 @@ import { NgxPaginationModule } from 'ngx-pagination';
     CommonModule,
     NgForOf,
     NavbarComponent,
-    NgOptimizedImage,
     NgxPaginationModule,
   ],
   templateUrl: './watchlist.component.html',
   styleUrls: ['./watchlist.component.css'],
 })
 export class WatchlistComponent implements OnInit {
-  stocks: any[] = [];
+  stocks: Stock[] = [];
   page: number = 1;
   itemsPerPage: number = 5;
 
-  constructor(private stockService: StockService) {}
+  constructor(private stockService: StockService, private sanitizer: DomSanitizer) {}
 
   ngOnInit(): void {
     this.stockService.getAllStocks().subscribe({
       next: (data) => {
-        this.stocks = data;
-        console.log('Fetched stocks:', data);
+        this.stocks = data.map(stock => ({
+          ...stock,
+          safeImageURL: this.sanitizer.bypassSecurityTrustUrl(stock.imageURL)
+        }));
+        console.log('Fetched stocks:', this.stocks);
       },
       error: (err) => console.error('Failed to fetch stocks', err),
     });
   }
+
 }
